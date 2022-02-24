da5d3f56b195eb119bf70a16cb95ac42dc6043a1
csdamo <csdamo.cx@gmail.com>
2022-02-24 18:20:38 -0300

    Try, one more time, fixed the program

--- code/app.py
+++ code/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
+from db import db
 
 from security import authenticate, identity
 from resources.item import Item, ItemList
@@ -13,6 +14,10 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'jose'
 api = Api(app)
 
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
 jwt = JWT(app, authenticate, identity)  # /auth
 
 api.add_resource(Item, '/item/<string:name>')
@@ -22,6 +27,5 @@ api.add_resource(StoreList, '/stores')
 api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':
-    from db import db
     db.init_app(app)
     app.run(port=5000, debug=True)
