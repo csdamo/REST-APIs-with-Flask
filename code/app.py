76caf67a9c9840131af414f352f644763372a2ad
csdamo <csdamo.cx@gmail.com>
2022-02-24 17:45:45 -0300

    Try to fix program running

--- code/app.py
+++ code/app.py
@@ -1,7 +1,6 @@
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
-from db import db
 
 from security import authenticate, identity
 from resources.item import Item, ItemList
@@ -23,5 +22,6 @@ api.add_resource(StoreList, '/stores')
 api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':
+    from db import db
     db.init_app(app)
     app.run(port=5000, debug=True)
